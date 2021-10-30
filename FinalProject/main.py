a499a6c1bc5ef7ebc6aec0484689ecbe10d4c9d7
Ravid-Nahmias <ravidnahmias@gmail.com>
2021-10-30 16:48:27 +0300

    Solving warnings

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,14 +1,14 @@
 from imageai.Detection import ObjectDetection
 import os
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path, "resnet50_coco_best_v2.1.0.h5"))
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path, "EnvFiles/yolo.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="C:/Users/MOEUser/Desktop/final project/yolov5-master/images/1.jpeg",
-output_image_path="C:/Users/MOEUser/PycharmProjects/pythonProject/imagenew.jpg")
+detections = detector.detectObjectsFromImage(input_image="photos/img.jpeg",
+output_image_path="photosNew/imagenew.jpg")
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
