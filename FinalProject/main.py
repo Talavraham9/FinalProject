687fd115a3a1a2bc6ece29e3de9e940a2fd379cf
Tal-Avraham <73162727+Tal-Avraham@users.noreply.github.com>
2021-10-30 09:58:12 +0300

    Change model to YOLO

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,14 +1,25 @@
 from imageai.Detection import ObjectDetection
 import os
+import tensorflow as tf
+import numpy as np
 
 execution_path = os.getcwd()
 
 detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path, "resnet50_coco_best_v2.1.0.h5"))
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath( os.path.join(execution_path, "EnvFiles/yolo.h5"))
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="C:/Users/MOEUser/Desktop/final project/yolov5-master/images/1.jpeg",
-output_image_path="C:/Users/MOEUser/PycharmProjects/pythonProject/imagenew.jpg")
+detections = detector.detectObjectsFromImage(input_image="1.jpeg",
+output_image_path="newimage.jpg")
+labelsPath = os.path.sep.join(["coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+    print(eachObject["box_points"])
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
