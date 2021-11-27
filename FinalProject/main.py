8edbd3242f6adb5347873a1261382002f701835a
Tal-Avraham <73162727+Tal-Avraham@users.noreply.github.com>
2021-11-27 22:12:19 +0200

    Create main

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,15 +1,41 @@
+#import
 from imageai.Detection import ObjectDetection
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import numpy as np
-execution_path = os.getcwd()
+import cv2
 
-detector = ObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path, "EnvFiles/yolo.h5"))
-detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="photos/img.jpeg",
-output_image_path="photosNew/imagenew.jpg")
 
-for eachObject in detections:
-    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+# ----------------------------------------------------------
+# MAIN
+# ----------------------------------------------------------
+if __name__ == '__main__':
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    model_path = "EnvFiles/yolo.h5"
+    input_path = "input/1.jpeg"
+    output_path = "output/out.jpg"
+
+    execution_path = os.getcwd()
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(model_path)
+    detector.loadModel(detection_speed="fast")
+    detections = detector.detectObjectsFromImage(input_image=input_path,
+    output_image_path=output_path)
+    detectedObject = []
+
+    image = cv2.imread(output_path)
+    #Prints the detected objects
+    for eachObject in detections:
+        print(eachObject)
+        print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+        print(eachObject["box_points"])
+        x= eachObject["box_points"][0]
+        y= eachObject["box_points"][1]
+        w=eachObject["box_points"][2]
+        h=eachObject["box_points"][3]
+
+        cv2.line(image, (x, y), (x, h), (0, 0, 255), 2)
+
+
+    cv2.imshow("output", image)                         # Show image
+    cv2.waitKey(0)                                      # Display the image infinitely until any keypress
