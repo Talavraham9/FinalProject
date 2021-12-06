9dce93fbaec5d766d48eff23af36900db5d3f90d
talavraham9 <talavraham@ception.ai>
2021-12-06 21:08:09 +0200

    Add option: Camera input

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -3,23 +3,11 @@ from imageai.Detection import ObjectDetection
 import os
 import numpy as np
 import cv2
-from matplotlib.patches import Polygon
 
 IMAGE = False
 VIDEO = True
-pts = Polygon(np.array([[0, 250], [250, 250],
-                [50, 50], [100, 100]],
-               np.int32).reshape((-1, 1, 2)))
-# pts = pts.reshape((-1, 1, 2))
-
-def drawPolygon(img):
-    color = (255, 255, 255)
-    thickness = 2
-    isClosed = True
-    cv2.polylines(img, [pts],
-                  isClosed, color, thickness)
-    return img
-# ---------------------------------------------------------------------
+CAMERA = True
+# # ---------------------------------------------------------------------
 # function      : detectFromImage
 # Description   : Detection objects from an image
 # ---------------------------------------------------------------------
@@ -52,7 +40,8 @@ def detectFromImage(input_path, output_path):
 # function      : detectFromVideo
 # Description   : Detection objects from a video
 # ---------------------------------------------------------------------
-def detectFromVideo(input_path):
+def detectFromVideo():
+    INPUT_FILE = "input/car.mp4"
     OUTPUT_FILE = 'output/videoDetections.avi'
     LABELS_FILE = 'C:/darknet-master/data/coco.names'
     CONFIG_FILE = 'C:/darknet-master/cfg/yolov3.cfg'
@@ -67,105 +56,61 @@ def detectFromVideo(input_path):
     np.random.seed(4)  # makes the random numbers predictable
     COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")  # Colors of the objects
     net = cv2.dnn.readNetFromDarknet(CONFIG_FILE, WEIGHTS_FILE)
+    if CAMERA:
+        cap = cv2.VideoCapture((0 + cv2.CAP_DSHOW))  # open camera
+    elif VIDEO:
+        cap = cv2.VideoCapture(INPUT_FILE)  # open our video
 
-    vs = cv2.VideoCapture(input_path)  # open our video
-    W = int(vs.get(3))
-    H = int(vs.get(4))
-    fps = vs.get(cv2.CAP_PROP_FPS)
-    # determine only the *output* layer names that we need from YOLO
-    ln = net.getLayerNames()
-    ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
-
-    cnt = 0;
     while True:
-        cnt += 1
-        # print("Frame number", cnt)
-        try:
-            (grabbed, image) = vs.read()
-        except:
-            break
-        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        ret, img = cap.read()
+        height, weight, _ = img.shape
+        blob = cv2.dnn.blobFromImage(img, 1 / 255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
         net.setInput(blob)
-        if W is None or H is None:
-            (H, W) = image.shape[:2]
-        layerOutputs = net.forward(ln)
 
-        # initialize our lists of detected bounding boxes, confidences, and
-        # class IDs, respectively
-        boxes = []
+        output = net.getUnconnectedOutLayersNames()
+        layers = net.forward(output)
+
+        box = []
         confidences = []
-        classIDs = []
+        class_ids = []
 
-        # loop over each of the layer outputs
-        for output in layerOutputs:
-            # loop over each of the detections
-            for detection in output:
-                # extract the class ID and confidence (i.e., probability) of
-                # the current object detection
+        for out in layers:  # loop over each of the layer outputs
+            for detection in out:  # loop over each of the detections
                 scores = detection[5:]
-                classID = np.argmax(scores)
-                confidence = scores[classID]
-
-                # filter out weak predictions by ensuring the detected
-                # probability is greater than the minimum probability
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
                 if confidence > CONFIDENCE_THRESHOLD:
-                    # scale the bounding box coordinates back relative to the
-                    # size of the image, YOLO actually returns the center (x, y)
-                    # -coordinates of the bounding box followed by the boxes'
-                    # width and height
-                    box = detection[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY, width, height) = box.astype("int")
+                    centre_x = int(detection[0] * weight)
+                    centre_y = int(detection[1] * height)
+                    w = int(detection[2] * weight)
+                    h = int(detection[3] * height)
 
-                    # use the center (x, y)-coordinates to derive the top and
-                    # and left corner of the bounding box
-                    x = int(centerX - (width / 2))
-                    y = int(centerY - (height / 2))
+                    x = int(centre_x - w / 2)
+                    y = int(centre_y - h / 2)
 
-                    # update our list of bounding box coordinates, confidences,
-                    # and class IDs
-                    boxes.append([x, y, int(width), int(height)])
+                    box.append([x, y, w, h])
                     confidences.append(float(confidence))
-                    classIDs.append(classID)
+                    class_ids.append(class_id)
 
-        # apply non-maxima suppression to suppress weak, overlapping bounding
-        # boxes
-        idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD,
-                                CONFIDENCE_THRESHOLD)
-
+        indexes = np.array(cv2.dnn.NMSBoxes(box, confidences, 0.5, 0.4))
         # ensure at least one detection exists
-        if len(idxs) > 0:
-            # loop over the indexes we are keeping
-            for i in idxs.flatten():
-                # extract the bounding box coordinates
-                (x, y) = (boxes[i][0], boxes[i][1])
-                (w, h) = (boxes[i][2], boxes[i][3])
+        for i in indexes.flatten():
+            x, y, w, h = box[i]
+            color = [int(c) for c in COLORS[class_ids[i]]]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.2f}".format(LABELS[class_ids[i]], confidences[i])
+            cv2.putText(img, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-                color = [int(c) for c in COLORS[classIDs[i]]]
-
-                cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
-                text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-                if pts.contains((x, y)):
-                    print ("IN!")
-                if pts.contains((w, h)):
-                    print("IN!")
-                # print("{} detect! {},{},{},{}".format(LABELS[classIDs[i]], boxes[i][0], boxes[i][1], boxes[i][0] + boxes[i][2], boxes[i][1] +boxes[i][3]))
-                cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-        # show the output image
-        drawPolygon(image)
-        cv2.imshow("output", image)
-        writer.write(image)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
+        cv2.imshow("output", img)  # show the output image
+        writer.write(img)
+        if cv2.waitKey(1) & 0xff == ord("q"):
             break
-
-    # do a bit of cleanup
-    cv2.destroyAllWindows()
 
     # release the file pointers
     print("[INFO] cleaning up...")
     writer.release()
-    vs.release()
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 # ----------------------------------------------------------
@@ -176,12 +121,12 @@ if __name__ == '__main__':
 
     input_path = "input/1.jpeg"
     output_path = "output/out.jpg"
-    video_input_path = "input/car.mp4"
+
 
     if IMAGE:
         detectFromImage(input_path, output_path)
 
-    elif VIDEO:
-        detectFromVideo(video_input_path)
+    elif VIDEO or CAMERA:
+        detectFromVideo()
 
     cv2.waitKey(0)  # Display the image infinitely until any keypress
