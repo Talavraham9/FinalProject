be3c8f3b8161733e46dc657e4b74ea45840a4f2e
Ravid-Nahmias <ravidnahmias@gmail.com>
2022-04-26 17:35:21 +0300

    server socket that wait for client connection and sent message when the connection established

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -1,15 +1,22 @@
 from imageai.Detection import ObjectDetection
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
+
 execution_path = os.getcwd()
+
+modelPath = "EnvFiles/yolo.h5"
+inputPath = "input/1.jpeg"
+outputPath = "output/newImage.jpg"
 
 detector = ObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath( os.path.join(execution_path, "EnvFiles/yolo.h5"))
-detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="photos/img.jpeg",
-output_image_path="photosNew/imagenew.jpg")
+detector.setModelPath(os.path.join(execution_path, modelPath))
+detector.loadModel(detection_speed="fast")
+
+detections = detector.detectObjectsFromImage(input_image=inputPath, output_image_path=outputPath)
 
 for eachObject in detections:
-    print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+    # if eachObject["name"] == 'car' or eachObject["name"] == 'motorcycle' or eachObject["name"] == 'bench':
+    print(eachObject["name"], " : ", eachObject["percentage_probability"])
